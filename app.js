b6aec1a9752d4b4daf3319eec3866bfd5e2afe96
Sérgio Calazans <88505877+Gunga-16@users.noreply.github.com>
2024-10-22 12:30:10 -0300

    k

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log('Olá, Mundo');
+
+
+function virarCartao(card){
+    const answer = card.querySelector('.resposta');
+    if (answer.style.display === 'none'){
+        answer.style.display = 'block';
+    } else {
+        answer.style.display = 'none';
+    }
+}
